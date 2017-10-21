54401c0e543b6805122f9c57c26e5750f7d2c5d0
Samseppiol <j_hage@live.com>
2017-10-21 11:00:41 +0100

    Classes and altering namespaces for classes in a separate folder

---
+++ Non-Primitive Types/Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes.Math;
+using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Doe";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
